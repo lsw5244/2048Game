546f0f01f29f46fc66efc8232950ca04b877809a
lsw <lsw5244@naver.com>
2020-08-19 14:51:37 +0900

    실행 시 처음 랜덤 스폰 구현, Restart버튼 구현
    실행 시 랜덤한 숫자 2개 생성되도록 구현
    Restart버튼 클릭 시 씬 다시불러오도록 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject[] n;
+
+    int x, y;
+
+    GameObject[,] square = new GameObject[4, 4];
+
     void Start()
     {
-        
+        Spawn();
+        Spawn();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 뒤로가기(종료)
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    // 다시 하기
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void Spawn()
+    {
+        while(true)
+        {
+            x = Random.Range(0, 4);
+            y = Random.Range(0, 4);
+
+            if(square[x, y] == null)
+            {
+                break;
+            }
+        }
+
+        square[x, y] = Instantiate(Random.Range(0, 8) > 0 ? n[0] : n[1]  // 1/8로 n[2], 7/8로 n[1]생성하도록 함
+            , new Vector3(-1.8f + 1.2f * x, -1.8f + 1.2f * y, 0),  // 위치
+            Quaternion.identity);
+        square[x, y].GetComponent<Animator>().SetTrigger("Spawn");
+
     }
 }
