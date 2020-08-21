03fffab54c68ff8281b5dcf95be7b9015e9c80ba
lsw <lsw5244@naver.com>
2020-08-21 14:56:35 +0900

    왼쪽으로 드레그했을 때의 블럭 움직임, 움직인 후 블럭 재생성 구현
    상 하 좌 우의 드레그를 입력 받을 수 있도록 구현
    왼쪽으로 드레그 했을 때 움직임, 움직인 후 블럭 재생성 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
 
     int x, y;
     Vector3 firstPos, gap;
+    bool wait, move;
 
     GameObject[,] square = new GameObject[4, 4];
 
@@ -29,19 +30,77 @@ public class GameManager : MonoBehaviour
         // 처음 터치 한 위치 저장  // 터치 한손으로 && 터치발생상태 == 터치시작
         if(Input.GetMouseButtonDown(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began))  // 확인
         {
+            wait = true;
             firstPos = Input.GetMouseButtonDown(0) ? Input.mousePosition : (Vector3)Input.GetTouch(0).position;
-
         }
         // 움직인 부분에서 처음 터치한 부분을 -하여 첫번째 터치와 두번째 터치의 gap 저장
         if(Input.GetMouseButton(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved))
         {
             gap = (Input.GetMouseButton(0) ? Input.mousePosition : (Vector3)Input.GetTouch(0).position) - firstPos;
-            Debug.Log(gap);
+            // Debug.Log(gap);
 
             if(gap.magnitude < 100)
             {
                 return;
             }
+            // gap을 -1 ~ 1 사이로 함
+            gap.Normalize();
+            // Debug.Log(gap);
+            if(wait)
+            {
+                wait = false;
+                // 위로 드레그
+                if (gap.y > 0 && gap.x > -0.5f && gap.x < 0.5f)
+                {
+                    //Debug.Log("위");
+                }
+                // 아래
+                else if (gap.y < 0 && gap.x > -0.5f && gap.x < 0.5f)
+                {
+                    //Debug.Log("아래");
+                }
+                // 오른쪽으로 >
+                else if (gap.x > 0 && gap.y > -0.5f && gap.y < 0.5f)
+                {
+                    //Debug.Log("오른쪽");
+                }
+                // 왼쪽 <
+                else if (gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f)
+                {
+                    //Debug.Log("왼쪽");
+                    for(y = 0; y <= 3; y++)
+                    {
+                        for(x = 3; x >= 1; x--)
+                        {
+                            for(int i = 0; i <= x-1; i++)
+                            {
+                                MoveOrCombine(i + 1, y, i, y);
+                            }
+                        }
+                    }
+                }
+                else return;
+                // 터치 했을 때 움직임이 있었으면 새로운 숫자 스폰시킴
+                if(move)
+                {
+                    Spawn();
+                    move = false;
+                }
+            }
+        }
+    }
+    // x1, y1 이동 전 좌표, x2, y2 이동 될 좌표
+    void MoveOrCombine(int x1, int y1, int x2, int y2)
+    {
+        // 이동 할 좌표가 비어있고 현재 좌표가 비어있지 않으면(이동할 수 있으면)
+        if(square[x2, y2] == null && square[x1, y1] != null)
+        {
+            //square[x1, y1].transform.position = Vector3.MoveTowards(square[x1, y1].transform.position,
+            //   new Vector3(-1.8f + 1.2f * x2, -1.8f + 1.2f * y2, 0), 0.1f);
+            move = true;
+            square[x1, y1].GetComponent<Move>().Moveing(x2, y2);
+            square[x2, y2] = square[x1, y1];
+            square[x1, y1] = null;
         }
     }
 
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    bool move = false;
+    int _x2, _y2;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(move)
+        {
+            Moveing(_x2, _y2);
+        }
+    }
+
+    public void Moveing(int x2, int y2)
+    {
+        move = true;
+        _x2 = x2;
+        _y2 = y2;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(-1.8f + 1.2f * x2, -1.8f + 1.2f * y2, 0), 0.3f);
+        // 목적지에 도착 하면 업데이트의 moving 작동 그만하도록 함
+        if(transform.position == new Vector3(-1.8f + 1.2f * x2, -1.8f + 1.2f * y2, 0))
+        {
+            move = false;
+        }
     }
 }
